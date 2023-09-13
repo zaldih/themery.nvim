bdd2d54d1bedf1e6455d00c6f6912c712c711ab7
Ryan Snyder <ryansnyder4@gmail.com>
2023-09-13 16:22:30 -0600

    feat: use a theme_id (position of the theme in the lua table) to determine what the current theme is
    When using themes that have different variations/flavors that are set using global variables, the cursor is not placed on the correct line in the menu buffer when opened. Similarly, the '>' is not always added to the beggining of the appropriate theme. This introduces a currentThemeId, which replaces the need for both currentThemeName and currentThemeIndex in controller.lua, while also leading to more accurate results.

--- lua/themery/controller.lua
+++ lua/themery/controller.lua
@@ -5,123 +5,133 @@ local window = require("themery.window")
 local api = vim.api
 
 local position = 0
-local currentThemeIndex = -1
-local currentThemeName = ""
 local resultsStart = constants.RESULTS_TOP_MARGIN
+local currentThemeId = vim.g.theme_id
 
 local function loadActualThemeConfig()
-  local themeList = config.getSettings().themes;
-  currentThemeName = vim.g.colors_name
+	local themeList = config.getSettings().themes
 
-  for k in pairs(themeList) do
-    if currentThemeName == themeList[k].colorscheme then
-      currentThemeIndex = k
-      position = k + resultsStart - 1
-      return
-    end
-  end
+	-- if currentThemeId isn't set, it's because it's the first time it has been run
+	if not currentThemeId then
+		position = resultsStart
+		return
+	end
+
+	for k in pairs(themeList) do
+		if currentThemeId == k then
+			position = k + resultsStart - 1
+			return
+		end
+	end
 end
 
 local function setColorscheme(theme)
-  if theme.before then
-    local fn, err = load(theme.before)
-    if err then
-      print("Themery error: "..err)
-      return false
-    end
-    if fn then fn() end
-  end
+	if theme.before then
+		local fn, err = load(theme.before)
+		if err then
+			print("Themery error: " .. err)
+			return false
+		end
+		if fn then
+			fn()
+		end
+	end
 
-  local ok, _ = pcall(
-    vim.cmd,
-    "colorscheme "..theme.colorscheme
-  )
+	local ok, _ = pcall(vim.cmd, "colorscheme " .. theme.colorscheme)
 
-  -- check if the colorscheme was loaded successfully
-  if not ok then
-    print(constants.MSG_ERROR.THEME_NOT_LOADED..": "..theme)
-    -- Restore previus
-    vim.cmd('colorscheme '..currentThemeName)
-    return false
-  end
+	-- check if the colorscheme was loaded successfully
+	if not ok then
+		print(constants.MSG_ERROR.THEME_NOT_LOADED .. ": " .. theme)
+		-- Restore previus
+		vim.cmd("colorscheme " .. config.getSettings().themes[currentThemeId])
+		return false
+	end
 
-  if theme.after then
-    local fn, err = load(theme.after)
-    if err then
-      print(constants.MSG_ERROR.GENERIC..": "..err)
-      return false
-    end
-    if fn then fn() end
-  end
+	if theme.after then
+		local fn, err = load(theme.after)
+		if err then
+			print(constants.MSG_ERROR.GENERIC .. ": " .. err)
+			return false
+		end
+		if fn then
+			fn()
+		end
+	end
 
-  return true
+	return true
 end
 
 local function updateView(direction)
-  local themeList = config.getSettings().themes;
-  position = position + direction
-  vim.api.nvim_buf_set_option(window.getBuf(), 'modifiable', true)
-  if position < resultsStart then position = resultsStart end
-  if position > #themeList + 1 then position = #themeList + 1 end
+	local themeList = config.getSettings().themes
+	position = position + direction
+	vim.api.nvim_buf_set_option(window.getBuf(), "modifiable", true)
+	if position < resultsStart then
+		position = resultsStart
+	end
+	if position > #themeList + 1 then
+		position = #themeList + 1
+	end
 
-  if #themeList == 0 then
-    window.printNoThemesLoaded()
-    vim.api.nvim_buf_set_option(window.getBuf(), 'modifiable', false)
-    return
-  end
+	if #themeList == 0 then
+		window.printNoThemesLoaded()
+		vim.api.nvim_buf_set_option(window.getBuf(), "modifiable", false)
+		return
+	end
 
-  local resultToPrint = {}
-  for i in ipairs(themeList) do
-    local prefix = '  '
+	local resultToPrint = {}
+	for i in ipairs(themeList) do
+		local prefix = "  "
 
-    if currentThemeIndex == i then
-      prefix = '> '
-    end
+		if currentThemeId == i then
+			prefix = "> "
+		end
 
-    resultToPrint[i] = prefix..themeList[i].name
-  end
+		resultToPrint[i] = prefix .. themeList[i].name
+	end
 
-  api.nvim_buf_set_lines(window.getBuf(), 1, -1, false, resultToPrint)
-  api.nvim_win_set_cursor(window.getWin(), {position, 0})
+	api.nvim_buf_set_lines(window.getBuf(), 1, -1, false, resultToPrint)
+	api.nvim_win_set_cursor(window.getWin(), { position, 0 })
 
-  if config.getSettings().livePreview then
-    setColorscheme(themeList[position-1])
-  end
+	if config.getSettings().livePreview then
+		setColorscheme(themeList[position - 1])
+	end
 
-  vim.api.nvim_buf_set_option(window.getBuf(), 'modifiable', false)
+	vim.api.nvim_buf_set_option(window.getBuf(), "modifiable", false)
 end
 
 local function revertTheme()
-  setColorscheme(config.getSettings().themes[currentThemeIndex])
+	-- setColorscheme(config.getSettings().themes[currentThemeIndex])
+	setColorscheme(config.getSettings().themes[currentThemeId])
 end
 
 local function open()
-  window.openWindow()
-  loadActualThemeConfig()
-  updateView(0)
+	window.openWindow()
+	loadActualThemeConfig()
+	updateView(0)
 end
 
 local function close()
-  window.closeWindow()
+	window.closeWindow()
 end
 
 local function closeAndRevert()
-  revertTheme()
-  window.closeWindow()
+	revertTheme()
+	window.closeWindow()
 end
 
 local function closeAndSave()
-  local theme = config.getSettings().themes[position-1]
-  persistence.saveTheme(theme)
-  window.closeWindow()
+	local theme = config.getSettings().themes[position - 1]
+	persistence.saveTheme(theme, position - 1)
+	currentThemeId = position - 1
+	window.closeWindow()
 end
 
 return {
-  open = open,
-  close = close,
-  closeAndRevert = closeAndRevert,
-  closeAndSave = closeAndSave,
-  updateView = updateView,
-  loadActualThemeConfig = loadActualThemeConfig,
-  setColorscheme = setColorscheme,
+	open = open,
+	close = close,
+	closeAndRevert = closeAndRevert,
+	closeAndSave = closeAndSave,
+	updateView = updateView,
+	loadActualThemeConfig = loadActualThemeConfig,
+	setColorscheme = setColorscheme,
 }
