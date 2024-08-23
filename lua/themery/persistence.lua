bb80d0e94149909573d8cd7a575ad2e94389a2ad
zaldih <imzaldih@gmail.com>
2024-08-23 15:07:27 +0200

    Improve filesystem operations
    Create a file for all IO operations and improve error handling related to this.

--- lua/themery/persistence.lua
+++ lua/themery/persistence.lua
@@ -1,7 +1,8 @@
 local constants = require("themery.constants")
-local config = require("themery.config")
 local utils = require("themery.utils")
-local state_path = vim.fn.stdpath("data") .. "/themery/state.json"
+local filesystem = require("themery.filesystem")
+local state_folder_path = vim.fn.stdpath("data") .. "/themery"
+local state_file_path = state_folder_path .. "/state.json"
 
 -- Saves the selected theme configuration to the user's config file.
 -- @param theme table containing the theme data
@@ -12,52 +13,60 @@ local function saveTheme(theme, theme_id)
         beforeCode = theme.before and utils.trimStartSpaces(theme.before) or "",
         colorscheme = theme.colorscheme,
         afterCode = theme.after and utils.trimStartSpaces(theme.after) or "",
-		theme_id = theme_id
+        theme_id = theme_id
     }
 
     local json_data = vim.json.encode(data)
 
-	-- Create plugin folder if not exist. :h remove the last part of the path
-	os.execute("mkdir -p " .. vim.fn.fnamemodify(state_path, ":h"))
-    local file = io.open(state_path, "w")
+    -- Create plugin folder if not exist.
+    local status, err = pcall(function()
+        return filesystem.createDirectoryIfNotExists(state_folder_path)
+    end)
 
-    if file == nil then
-        print(constants.MSG_ERROR.WRITE_FILE .. ": " .. state_path)
+    if not status then
+        print(constants.MSG_ERROR.CREATE_DIRECTORY .. ": " .. state_folder_path)
         return
     end
 
-    file:write(json_data)
-    file:close()
+    local status, err = pcall(function()
+        return filesystem.writeToFile(state_file_path, json_data)
+    end)
+
+    if not status then
+        print(constants.MSG_ERROR.WRITE_FILE .. ": " .. err)
+        return
+    end
+
     print(constants.MSG_INFO.THEME_SAVED)
 end
 
 local function loadState()
-    local file = io.open(state_path, "r")
+    local status, json_data = pcall(function()
+        return filesystem.readFromFile(state_file_path)
+    end)
 
-    if file == nil then
-        -- No config file found
+    if not status then
+        -- The file not exist. It's the first time the plugin is run so
+        -- nothing to do.
         return
     end
 
-    local json_data = file:read("*all")
-    file:close()
-
     local data = vim.json.decode(json_data)
 
-	-- Set a global variable in vim
-	vim.g.theme_id = data.theme_id
-	local fn, err = load(data.beforeCode)
-	if fn then
-		fn()
-	end
+    -- Set a global variable in vim
+    vim.g.theme_id = data.theme_id
+    local fn, err = load(data.beforeCode)
+    if fn then
+        fn()
+    end
     vim.cmd("colorscheme " .. data.colorscheme)
-	local fn, err = load(data.afterCode)
-	if fn then
-		fn()
-	end
+    local fn, err = load(data.afterCode)
+    if fn then
+        fn()
+    end
 end
 
 return {
-	saveTheme = saveTheme,
-	loadState = loadState,
+    saveTheme = saveTheme,
+    loadState = loadState,
 }
